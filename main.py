557c546536e7e9beda5f8dea0dd46415951b8b4e
Marwa Jarad <marwajarad0@gmail.com>
2025-11-20 16:55:28 +0000

    Added square, power to x

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ def power_x(x, y):
 def square(x): 
     return math.sqrt(x)
 
+
+
 user_input = input("""Addition : a
 Subtraction : s
 Multiplication : m
@@ -26,42 +28,58 @@ Divison : d
 Square : sq
 To the power of x: p
  """)
-    
-if user_input.lower() == "a": 
-    x = int(input("Add x")) 
-    y = int(input("Add y"))
-    print(addition(x, y))
 
-elif user_input.lower() == "s": 
-    x = int(input("Add x"))
-    y = int(input("Add y"))    
-    print(subratction(x, y))
+while user_input:
+   if user_input.lower() == "a": 
+      x = int(input("Add x")) 
+      y = int(input("Add y"))
+      value = addition(x, y)
+      print(value)
 
-elif user_input.lower() == "m": 
-   x = int(input("Add x"))
-   y = int(input("Add y"))
-   print(multiplication(x, y))
+   elif user_input.lower() == "s": 
+       x = int(input("Add x"))
+       y = int(input("Add y"))    
+       value = subratction(x, y)
+       print(value)
 
-elif user_input.lower() == "d": 
-    x = int(input("Add x"))
-    y = int(input("Add y"))
-    print(divison(x, y))
+   elif user_input.lower() == "m": 
+       x = int(input("Add x"))
+       y = int(input("Add y"))
+       value = multiplication(x, y)
+       print(value)
+   elif user_input.lower() == "d": 
+       x = int(input("Add x"))
+       y = int(input("Add y"))
+       value = divison(x, y)
+       print(value)
 
-elif user_input.lower() == "p": 
-    x = int(input("Whats your base?"))
-    y = int(input("Whats your exponent?"))
-    print(power_x(x, y))
+   elif user_input.lower() == "p": 
+       x = int(input("Whats your base?"))
+       y = int(input("Whats your exponent?"))
+       value = power_x(x, y)
+       print(value)
 
-elif user_input.lower() == "sq": 
-    x = int(input("What would you like to square?"))
-    print(square(x))
+   elif user_input.lower() == "sq": 
+       x = int(input("Num to square: "))
+       value = square(x)
+       print(value)
+   else: 
+      print("""Entre a valid letter!!!
+       Addition : a
+       Subtraction : s
+       Multiplication : m
+       Divison : d
+       Square : sq
+       To the power of x: p""")
 
-else: 
-    print("Entre a valid letter!!!")
-    print("Addition : a ")
-    print("Subtraction : s")
-    print("Multiplication : m")
-    print("Divison : d")
-    print("Square : s")
-    print("To the power of x: p")
+   again = input("Continue? (yes/no)")
+   if again.lower() == "yes": 
+         user_input = input("""Addition : a
+         Subtraction : s
+         Multiplication : m
+         Divison : d
+         Square : sq
+         Power of x: p""")
+   else: 
+      break
 
